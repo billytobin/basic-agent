09edabd3b454b179ac7446c7b85db810e7dfcf2c
Billy Tobin <williamrtobin1@gmail.com>
2025-05-18 22:15:59 -0400

    tooling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wikipedia_tool
 
 
 load_dotenv()
@@ -36,12 +37,20 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
+tools = [search_tool, wikipedia_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools = []
+    tools = tools
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
-raw_response = agent_executor.invoke({"query": "What is the impact of climate change on polar bears?"})
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("Enter your research query: ")
+raw_response = agent_executor.invoke({"query": query})
 print(raw_response)
+
+try:
+    structured_response = parser.parse(raw_response["output"][0]["text"])
+    print(structured_response)
+except Exception as e:
+    print(f"Error parsing response: {e}")
