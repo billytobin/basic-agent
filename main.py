1f44fba3a65cb261f6cc7bda794f8d442da1632d
Billy Tobin <williamrtobin1@gmail.com>
2025-05-18 22:37:56 -0400

    fixed tools list

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from tools import search_tool, wikipedia_tool
+from tools import search_tool, wikipedia_tool, save_tool
 
 
 load_dotenv()
@@ -37,7 +37,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
-tools = [search_tool, wikipedia_tool]
+tools = [search_tool, wikipedia_tool, save_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
